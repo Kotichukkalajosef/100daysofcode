a1ff13974c25ba7e8fabf2053fcf1bd94a321d01
josef <josefkotichuakkala@gmail.com>
2023-09-14 20:34:26 +0530

    second commit

--- day1/src/Main.java
+++ day1/src/Main.java
@@ -1,13 +1,43 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int x; //declaration
-         x = 10; //assign
-        int z = 100; // initilazation
-        System.out.println("my name num is "+x);
-        System.out.println(z);
-        String name = "Josef";
+//        varibles
+
+//        int x; //declaration
+//         x = 10; //assign
+//        int z = 100; // initilazation
+//        System.out.println("my name num is "+x);
+//        System.out.println(z);
+//        String name = "Josef";
+//        System.out.println("Hello "+name);
+
+        //swap two variables
+
+//        String x = "Josef";
+//        String y = "Kotichukkala";
+//        String temp;
+//        temp = x;
+//        x = y;
+//        y = temp;
+//
+//        System.out.println(x);
+//        System.out.println(y);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter age: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+        System.out.println("food: ");
+        String food = sc.nextLine();
+        System.out.println(food);
+        System.out.println("age is "+age);
         System.out.println("Hello "+name);
+
+
 
 
     }
