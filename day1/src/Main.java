a3fb775964e839f190ac180d138bb479242a537f
josef <josefkotichuakkala@gmail.com>
2023-09-14 19:42:42 +0530

    first commit

---
+++ day1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x; //declaration
+         x = 10; //assign
+        int z = 100; // initilazation
+        System.out.print("my name num is "+x);
+
+
+    }
+}
